b055094dcfb7e43d5e14f1df26a06971db94d304
swang <swang@kaikeba.com>
2015-05-22 16:26:24 +0800

    add test script

---
+++ src/test/java/org/kkb/server/api/restAssured/courseVersion/GetCourseVsersionsTest.java
@@ -0,0 +1,37 @@
+package org.kkb.server.api.restAssured.courseVersion;
+
+import com.jayway.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.kkb.server.api.TestConfig;
+import org.testng.annotations.Test;
+
+
+/**
+ *获取相同type的数据
+ * ws.wang
+ */
+@Test
+public class GetCourseVsersionsTest {
+    //token 丢失
+    public void testWithNoToken(){
+        Response response= TestConfig.getOrDeleteExecu("get", "/course_versions?type=InstructiveCourse");
+        response.then().
+                assertThat().statusCode(400);
+    }
+    //token 失效
+    public  void testWithErrorToken(){
+        Response response= TestConfig.getOrDeleteExecu("get", "/course_versions?access_token=56789klkjhgfghjkt&type=InstructiveCourse");
+        response.then().
+                assertThat().statusCode(401);
+    }
+    //正确查找
+    public void testSuc(){
+        String token=TestConfig.getToken("/kauth/authorize?uid=812277&cid=www&tenant_id=1");
+        Response response= TestConfig.getOrDeleteExecu("get", "/course_versions?access_token="+token+"&type=InstructiveCourse");
+        response.then().
+                assertThat().statusCode(200).
+                body("data",Matchers.notNullValue()).
+                body("data.size()",Matchers.is(101));
+    }
+
+}
